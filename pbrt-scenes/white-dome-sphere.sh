5fdc7dabf03b4ec0d0d0e63ef0eed8abff92f010
Chun-Po Wang <artoo@chimaera.(none)>
2014-07-24 00:17:21 -0700

    Added white dome test file

---
+++ pbrt-scenes/white-dome-sphere.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+NAME="white-dome-sphere"
+PBRT_FILE="${NAME}.pbrt"
+EXR_FILE="${NAME}.exr"
+OUTPUT_DIR="${NAME}"
+
+DISPLAY_CMD="i_view32"
+
+# Determine if display command is available
+if ! which "$DISPLAY_CMD"; then
+    DISPLAY_CMD="eog"
+    if ! which "$DISPLAY_CMD"; then
+        DISPLAY_CMD="display"
+    fi
+fi
+
+mkdir -p "${OUTPUT_DIR}"
+
+# Search for unused sequence number
+for i in $(seq -f '%03g' 0 999); do
+    TIF_FILE="${NAME}.${i}.tif"
+    if [ ! -e "${OUTPUT_DIR}/$TIF_FILE" ]; then
+        break
+    fi
+done
+
+if [ -e "${OUTPUT_DIR}/$TIF_FILE" ]; then
+    echo >&2 "All sequence numbers are used. Abort"
+    exit -1
+fi
+
+echo >&2 "Write to ${OUTPUT_DIR}/${TIF_FILE}"
+
+pbrt "$PBRT_FILE"
+exrtotiff "$EXR_FILE" "${OUTPUT_DIR}/${TIF_FILE}"
+"$DISPLAY_CMD" "${OUTPUT_DIR}/${TIF_FILE}"
+
