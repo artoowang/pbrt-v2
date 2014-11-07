3a78b6cb609fa0f7484204a447bccbc1be86a739
Chun-Po Wang <Artoo@Chimaera.(none)>
2014-11-07 10:20:25 -0800

    Add Fresnel to diffuse

--- matlab/Heitz/f_G1_GGX_diffuse.m
+++ matlab/Heitz/f_G1_GGX_diffuse.m
@@ -1,3 +1,3 @@
-function vals = f_G1_GGX_diffuse(wis, wo, alpha, f0)
-    vals = f_G1_diffuse(wis, wo, @(w) D_GGX(w, alpha), @(wo, wn, D) G1_GGX(wo, wn, alpha));
+function vals = f_G1_GGX_diffuse(wis, wo, alpha, F)
+    vals = f_G1_diffuse(wis, wo, @(w) D_GGX(w, alpha), @(wo, wn, D) G1_GGX(wo, wn, alpha), F);
 end
--- matlab/Heitz/f_G1_diffuse.m
+++ matlab/Heitz/f_G1_diffuse.m
@@ -1,6 +1,10 @@
 % wo: 3x1 vector
 % wis: 3xN vectors
-function val = f_G1_diffuse(wis, wo, D, G1)
+% D: @(w)
+% G1: @(wo, wn, D)
+% F: @(costheta)
+% vals: 1xN vector
+function val = f_G1_diffuse(wis, wo, D, G1, F)
     N = size(wis, 2);
     val = zeros(1, N);
     
@@ -14,7 +18,7 @@ function val = f_G1_diffuse(wis, wo, D, G1)
             % Method 1: quad2d
             %
             val(i) = quad2d(@(theta, phi) integrandOverWmThetaPhi(...
-                theta, phi, wis(:, i), wo, D, G1), ...
+                theta, phi, wis(:, i), wo, D, G1, F), ...
                 0, pi, 0, 2*pi);
             %}
             % Method 2: Monte Carlo
@@ -24,7 +28,7 @@ function val = f_G1_diffuse(wis, wo, D, G1)
             %   function so is therefore hard coded
             %{
             val(i) = monteCarloIntegrate(...
-                @(w) integrandOverWm(w, wis(:, i), wo, D, G1), ...
+                @(w) integrandOverWm(w, wis(:, i), wo, D, G1, F), ...
                 ... %@Sample_UniformSphere,
                 @(N) Sample_D_GGX(N, 0.1), ...
                 1000);
@@ -35,20 +39,24 @@ function val = f_G1_diffuse(wis, wo, D, G1)
     end
 end
 
-function val = integrandOverWm(wm, wi, wo, D, G1)
-    wo_dot_wm_clamped = wo' * wm;  % 1xN
-    wo_dot_wm_clamped(wo_dot_wm_clamped < 0) = 0;
-    wi_dot_wm_clamped = wi' * wm;  % 1xN
-    wi_dot_wm_clamped(wi_dot_wm_clamped < 0) = 0;
+% wo, wi: 3x1 vector
+function val = integrandOverWm(wm, wi, wo, D, G1, F)
+    wo_dot_wm = wo' * wm;  % 1xN
+    wo_dot_wm_clamped = wo_dot_wm;
+    wo_dot_wm_clamped(wo_dot_wm < 0) = 0;
+    wi_dot_wm = wi' * wm;  % 1xN
+    wi_dot_wm_clamped = wi_dot_wm;
+    wi_dot_wm_clamped(wi_dot_wm < 0) = 0;
     
     G1_vals = G1(wo, wm, D);
     D_vals = D(wm);
+    F_vals = F(wi_dot_wm);
     
-    val = wo_dot_wm_clamped .* wi_dot_wm_clamped .* G1_vals .* D_vals;
+    val = wo_dot_wm_clamped .* wi_dot_wm_clamped .* G1_vals .* D_vals .* F_vals;
 end
 
-function val = integrandOverWmThetaPhi(theta, phi, wi, wo, D, G1)
+function val = integrandOverWmThetaPhi(theta, phi, wi, wo, D, G1, F)
     wm = sph2vector(theta, phi);
-    val = integrandOverWm(wm, wi, wo, D, G1) .* sin(theta(:)');
+    val = integrandOverWm(wm, wi, wo, D, G1, F) .* sin(theta(:)');
     val = reshape(val, size(theta));
 end
